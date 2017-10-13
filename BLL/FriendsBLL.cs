7f91628230cb6aad4d583bb63ab44705d4d17fa7
ludw <ludw@southmoney.com>
2017-10-13 15:47:32 +0800

    444

---
+++ BLL/FriendsBLL.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class FriendsBLL
+    {
+
+        FriendsDAL oFriendsDAL = new FriendsDAL();
+
+        /// <summary>
+        /// 获取好友列表
+        /// </summary>
+        /// <param name="UserId">当前用户Id</param>
+        /// <returns></returns>
+        public object Friend_GetFriendsList(Guid UserId)
+        {
+
+            DataSet ds = oFriendsDAL.Friend_GetFriendsList(UserId);
+            return Common.JsonHelper.ToJson_L(ds,0,0,false);
+
+        }
+
+    }
+}
---
+++ BLL/HomeBLL.cs
@@ -0,0 +1,43 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace BLL
+{
+    public class HomeBLL
+    {
+        HomeDAL oHomeDAL = new HomeDAL();
+
+
+        /// <summary>
+        /// 登陆验证
+        /// </summary>
+        /// <param name="account">账号</param>
+        /// <param name="pswd">密码</param>
+        /// <returns></returns>
+        public object LoginCheck(string account, string pswd)
+        {
+            DataSet ds = oHomeDAL.LoginCheck(account, pswd);
+            return Common.JsonHelper.ToJson_L(ds);
+        }
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="model">注册信息</param>
+        /// <returns></returns>
+        public object Register(UserInfo model)
+        {
+            DataSet ds = oHomeDAL.Register(model);
+            return Common.JsonHelper.ToJson_L(ds);
+        }
+
+
+
+    }
+}
